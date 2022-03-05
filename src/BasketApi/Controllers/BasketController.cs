a87e53853a6efa51ba5a43cf48d7374d12646abb
Dionis-S <dionisciosepulveda@gmail.com>
2022-03-05 14:59:08 -0800

    basket

---
+++ src/BasketApi/Controllers/BasketController.cs
@@ -0,0 +1,52 @@
+﻿using BasketApi.Entities;
+using BasketApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+
+        [HttpGet("{userName}",Name = "GetBasket")]
+        public async Task<ActionResult> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+
+            return Ok(await _repository.UpdateBasket(basket));
+        
+        }
+
+        [HttpDelete("{userName}",Name ="DeleteBasket")]
+        public async Task<ActionResult> DeleteBasket(String userName)
+        {
+
+            await _repository.DeleteBasket(userName);
+            return Ok();
+
+        }
+
+
+
+    }
+}
---
+++ src/BasketApi/Repositories/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using BasketApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketApi.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(String userName);
+
+    }
+}
